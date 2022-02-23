0a0a3d2aed54928f9d5b33184a0bbb6526801755
Siguhau <siguhau@stud.ntnu.no>
2022-02-23 10:48:44 +0100

    Tried to add fade, commented out

--- lyden_av_byen/src/AudioMarkers.tsx
+++ lyden_av_byen/src/AudioMarkers.tsx
@@ -13,10 +13,15 @@ export type AudioMarker = {
 
   const AudioMarkersMap = () => {
     
+    const [volume, setVolume] = useState(0);
+
     const [url, setUrl] = useState("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3");
     const [audio, setAudio] = useState(new Audio(url));
+    
     const [playing, setPlaying] = useState(true);
     const [marker, setMarker] = useState("");
+    const [fadeInInterval, setFadeIninterval] = useState<NodeJS.Timer| undefined>();
+    const [fadeOutInterval, setFadeOutinterval] = useState<NodeJS.Timer| undefined>();
 
     async function getAudioMarkers() {
       const response = await collection<AudioMarker>('AudioMarkers').get();
@@ -25,11 +30,77 @@ export type AudioMarker = {
   }
 
     const [audioMarkers, setAudioMarkers] = useState<AudioMarker[]>([]);
+
+/*
+
+    useEffect(()=>{
+      audio.loop = true;
+    },[audio])
+
+    useEffect(()=>{
+      setTimeout(()=>{
+        if (volume !== 1)
+        setVolume(volume + 0.2);
+      }, 1000)
+      if(volume ===1 && fadeInInterval){
+        
+        clearInterval(fadeInInterval)
+        setFadeIninterval(undefined);
+      }
+      if(volume === 0 && fadeOutInterval){
+        
+        clearInterval(fadeOutInterval)
+        setFadeIninterval(undefined);
+      }
+      
+
+    }, [volume])
+
+
+    function fadeAudioIn() {
+      const timer = setInterval(()=>{
+        if (volume !== 1){
+          setVolume(volume+0.2)
+          console.log("fade in: " + volume.toFixed(2))
+          audio.volume = volume
+        }
+      }, 1000 )
+      setFadeIninterval(timer)
+    }
+
+    function fadeAudioOut(){
+      const timer = setTimeout(()=>{
+        if (volume !== 0){
+          setVolume(volume-0.2)
+          console.log("fade out: " + volume.toFixed(2))
+          audio.volume = volume
+        }else {
+          audio.pause()
+        }
+      }, 1000 )
+      setFadeOutinterval(timer)
+    }
+*/
+    useEffect(()=>{
+
+      if (playing === true){
+        audio.play()
+        //fadeAudioIn()
+      } else{
+        //fadeAudioOut();
+        audio.pause()
+      }
+      
+    },[playing])
+
+/*
     useEffect(() => {
         playing ? audio.play() : audio.pause();
+
         },
         [playing]
     );
+    */
 
     useEffect(()=>{
       getAudioMarkers().then((audioMarkers) => {
