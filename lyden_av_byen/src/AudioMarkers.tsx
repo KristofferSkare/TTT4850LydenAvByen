c616c11196d2682d8539da8f0682e8c1d864b18b
KristofferSkare <kristoffer.skare@live.com>
2022-03-16 12:10:27 +0100

    Cross fading with self and looping

--- lyden_av_byen/src/AudioMarkers.tsx
+++ lyden_av_byen/src/AudioMarkers.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Marker } from "react-leaflet";
 import { collection } from './firebase'
 
@@ -6,15 +6,18 @@ import { collection } from './firebase'
 export type AudioMarker = {
     id: string,
     audioUrl: string,
-    latitude : number,
-    longitude : number
+    preAudioUrl?: string,
+    latitude: number,
+    longitude: number
   }
 
 
 const AudioMarkersMap = () => {
-  
-  const [audio, setAudio] = useState(new Audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"));
-  const [playing, setPlaying] = useState(false);
+  const crossFadeTime = 2.3;
+  const [preAudio, setPreAudio] = useState<null | HTMLAudioElement>(null)
+  const [audio, setAudio] = useState<null | HTMLAudioElement>(null);
+  const [audio2, setAudio2] = useState<null | HTMLAudioElement>(null);
+  const playingRef = useRef(false);
   
   
   const [marker, setMarker] = useState("");
@@ -24,27 +27,121 @@ const AudioMarkersMap = () => {
   async function getAudioMarkers() {
     const response = await collection<AudioMarker>('AudioMarkers').get();
     return response.docs.map(doc => ({id: doc.id, audioUrl: doc.data().audioUrl, 
-      latitude: doc.data().latitude, longitude: doc.data().longitude}));
+      latitude: doc.data().latitude, longitude: doc.data().longitude, preAudioUrl: doc.data().preAudioUrl}));
 }
 
   const [audioMarkers, setAudioMarkers] = useState<AudioMarker[]>([]);
 
-  useEffect(()=>{
-    if (playing){
+  const play = () => {
+    if (preAudio) {
+      console.log("Starting pre audio")
+      preAudio.play()
+    } else if (audio) { 
+      console.log("Starting audio 1")
       audio.play()
+    } else {
+      playingRef.current = false;
     }
-    else {
+  }
+
+  const stop = () => {
+    if (preAudio) {
+      preAudio.ontimeupdate = () => {}
+  
+    }
+    if (audio) {
+      audio.ontimeupdate = () => {}
+  
+    }
+    if (audio2) {
+      audio2.ontimeupdate = () => {}
+  
+    }
+    if (preAudio) {
+      preAudio.pause()
+    }
+    if (audio) {
       audio.pause()
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[playing])
+    if (audio2) {
+      audio2.pause()
+    }
+  }
 
+  useEffect(() => {
+    if (!playingRef.current) {
+      stop()
+    }
+  },[playingRef.current])
 
   useEffect(()=>{
     getAudioMarkers().then((audioMarkers) => {
       setAudioMarkers(audioMarkers);
     });
   },[]);
+
+  const makeAudioObjects = (url: string, preUrl: string | undefined) => {
+    const a = new Audio(url);
+    const a2 = new Audio(url);
+    let pa = null;
+    if (preUrl) {
+      pa = new Audio(preUrl);
+      pa.ontimeupdate = (time) => {
+        const targetAudio = time.target as HTMLAudioElement;
+        if (targetAudio.duration - targetAudio.currentTime <= crossFadeTime) {
+          if (a.paused && playingRef.current) {
+            a.play()
+            console.log("Starting audio 1")
+          }
+        }
+      }
+      pa.oncanplaythrough = (e) => {
+        const targetAudio = e.target as HTMLAudioElement;
+        targetAudio.play()
+      }
+    } else {
+      a.oncanplaythrough = (e) => {
+        const targetAudio = e.target as HTMLAudioElement;
+        targetAudio.play()
+      }
+    }
+
+    a.ontimeupdate = (time) => {
+      const targetAudio = time.target as HTMLAudioElement;
+      if (targetAudio.duration - targetAudio.currentTime <= crossFadeTime) {
+        if (a2.paused && playingRef.current) {
+          a2.play()
+          console.log("Starting audio 2")
+        }
+      }
+    }
+    a2.ontimeupdate = (time) => {
+      const targetAudio = time.target as HTMLAudioElement;
+      if (targetAudio.duration - targetAudio.currentTime <= crossFadeTime) {
+        if (a.paused && playingRef.current) {
+          a.play()
+          console.log("Starting audio 1")
+        }
+      }
+    }
+    return {audio: a, audio2: a2, preAudio: pa};
+  }
+  const markerOnClick = (audioMarker: AudioMarker) => {
+    stop()
+    const markerId = audioMarker.id;
+
+    if (!playingRef.current || markerId !== marker) {
+      const {audio, audio2, preAudio} = makeAudioObjects(audioMarker.audioUrl, audioMarker.preAudioUrl)
+      setAudio(audio);
+      setAudio2(audio2)
+      setPreAudio(preAudio);
+      playingRef.current = true
+    } else {
+      playingRef.current = false
+    }
+
+    setMarker(markerId)
+}
 
   const audioMarkersMap = audioMarkers.map((audioMarker) => {
     
@@ -54,16 +151,7 @@ const AudioMarkersMap = () => {
           position={[audioMarker.latitude, audioMarker.longitude]}  
           eventHandlers={{
               click: () => {
-                  const markerID = audioMarker.id;
-                  setPlaying(false);
-                  setAudio(new Audio(audioMarker.audioUrl));
-                  setPlaying(marker !== markerID);
-                  if (marker !== markerID){
-                      setPlaying(true);
-                  } else {
-                      setPlaying(!playing);
-                  }
-                  setMarker(markerID); 
+                markerOnClick(audioMarker)
               },
           }}>
           </Marker>
